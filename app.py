52cc5442998a82f2fa7a77d49452595f67b9ffa5
MakineniSuryaTej <64590688+MakineniSuryaTej@users.noreply.github.com>
2021-06-25 00:07:42 +0530

    changes

--- app.py
+++ app.py
@@ -3,8 +3,28 @@ from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/bread')
+def bread():
+    return render_template('bread.html')
+
+@app.route('/drinks')
+def drinks():
+    return render_template('drinks.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
 
 if __name__=='__main__':
     app.run(debug=True)
